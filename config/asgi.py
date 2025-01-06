d383e6200feeeae3db23e3bd4d8e2cb7033de641
Usman-Nexquery <usmanshabbir@nexquery.com>
2025-01-06 16:18:44 +0500

    CH-4 authentication logic is now handleding in middleware

--- config/asgi.py
+++ config/asgi.py
@@ -1,8 +1,9 @@
 import os
-
 import django
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'config.settings')
 django.setup()
+
+from apps.users.middleware import TokenAuthenticationMiddleware
 from django.core.asgi import get_asgi_application
 from channels.routing import ProtocolTypeRouter, URLRouter
 from channels.auth import AuthMiddlewareStack
@@ -11,7 +12,7 @@ from apps.chat.routing import websocket_urlpatterns
 
 application = ProtocolTypeRouter({
     "http": get_asgi_application(),
-    "websocket": AuthMiddlewareStack(
+    "websocket": TokenAuthenticationMiddleware((AuthMiddlewareStack(
         URLRouter(websocket_urlpatterns)
-    ),
+    ))),
 })
